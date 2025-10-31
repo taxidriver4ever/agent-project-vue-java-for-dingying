87e3bf2b46a31da308a973e91118ee49bb3907eb
ym f <544467528qq@gmail.com>
2025-10-31 19:08:10 +0800

    我是sb

--- ai-front-end/src/component/Register.vue
+++ ai-front-end/src/component/Register.vue
@@ -6,12 +6,12 @@
     <el-asider class="left-asider"></el-asider>
     <el-footer></el-footer>
 </el-container> -->
-    <div style="display: flex;height: 100vh;">
+    <div style="display: flex;height: 100vh;width: 100vw;">
         <ElContainer style="display: flex;height: 100vh;width: 100%;">
-            <ElMain style="display: flex; height: 100vh;flex: 1;">
+            <ElMain style="display: flex; height: 100vh;flex: 1 1 50%;">
                 <div style="display: flex;flex-direction: column;width: 100%;justify-content: center;align-items: center;">
                     <h2 style="display: flex;">用邮箱继续</h2>
-                    <Input v-model="email" placeholder="请输入邮箱"
+                    <input v-model="email" placeholder="请输入邮箱"
                         style="display: flex; border: 1px solid #7c2cf2; border-radius: 8px;padding: 10px;margin-top: 10px;" />
                     <input v-model="password" placeholder="请输入密码"
                         style="display: flex; border: 1px solid #7c2cf2; border-radius: 8px;padding: 10px;margin-top: 50px;margin-bottom: 50px;" />
@@ -19,7 +19,7 @@
                         style="display: flex; border: 1px solid #7c2cf2; border-radius: 8px;padding: 10px;" />
                 </div>
             </ElMain>
-            <ElAside style="overflow: hidden;display: flex;height: 100vh;flex: 1;">
+            <ElAside style="overflow: hidden;display: flex;height: 100vh;flex: 1 1 50%;">
                 <ElImage style="display: flex;" src="/src/image/right.png" alt=""></ElImage>
             </ElAside>
         </ElContainer>
